0648c4c378cb7178077b0a2dd9ce14bc97ad312b
arya <aryabhattarai180@gmail.com>
2023-06-04 22:56:33 +0545

    snake's direction can now be changed from another program

--- snake-rl/snake.py
+++ snake-rl/snake.py
@@ -1,5 +1,6 @@
 import random
 import pygame
+from enum import Enum
 
 pygame.init()
 
@@ -12,6 +13,13 @@ WHITE = (255,255,255)
 BLACK = (0,0,0)
 
 class Snake :
+
+    class Directions(Enum) :
+        UP = [0, -1]
+        DOWN = [0, 1]
+        LEFT = [-1, 0]
+        RIGHT = [1, 0]
+    
     def __init__(self, speed=15, screen_width = 1000, screen_height = 600) :
         # Setup pygame
         self.swidth = screen_width
@@ -27,40 +35,23 @@ class Snake :
         # Snake Details
         self.snake_length = 1
         self.snake_size = 20
-        self.snake_speed = self.snake_size 
+        self.snake_speed = self.snake_size
+        
         # self.snake_speed = 25
         self.snake_spawn = [self.swidth // 2, self. sheight // 2]   # snake spawn position
         self.snake_pos = self.snake_spawn           # will be used for storing snake position at certain time
         self.snake_body = [self.snake_pos]
-        self.direction = [-1, 0]                     # Snake direction, left by default
+
+        # Snake direction, left by default 
+        self.direction = self.Directions.LEFT
+
         self.snake_color = [DARK_GREEN, GREEN]
 
         # Food Details
         self.food_pos = [random.randint(0,self.swidth - self.snake_size)//self.snake_size * self.snake_size, random.randint(0,self.sheight - self.snake_size)//self.snake_size * self.snake_size]
         self.food_size = self.snake_size
         self.food_color = RED
-
-
-    def reset_game(self) :
-        # Game Details:
-        self.score = 0
-
-        # Snake Details
-        self.snake_length = 1
-        self.snake_size = 20
-        self.snake_speed = self.snake_size 
-        # self.snake_speed = 25
-        self.snake_spawn = [self.swidth // 2, self. sheight // 2]   # snake spawn position
-        self.snake_pos = self.snake_spawn           # will be used for storing snake position at certain time
-        self.snake_body = [self.snake_pos]
-        self.direction = [-1,0]                     # Snake direction, left by default
-        self.snake_color = [DARK_GREEN, GREEN]
-
-        # Food Details
-        self.food_pos = [random.randint(0,self.swidth - self.snake_size)//self.snake_size * self.snake_size, random.randint(0,self.sheight - self.snake_size)//self.snake_size * self.snake_size]
-        self.food_size = self.snake_size
-        self.food_color = RED
-
+        
     def draw_snake(self):
         for pos in self.snake_body :
             pygame.draw.rect(self.screen, DARK_GREEN, pygame.Rect(pos[0], pos[1], self.snake_size, self.snake_size))
@@ -69,17 +60,53 @@ class Snake :
     def spawn_food(self) :
         pygame.draw.rect(self.screen, RED, pygame.Rect(self.food_pos[0], self.food_pos[1], self.snake_size, self.snake_size))
 
-    def get_direction(self) : 
-        # A function like this could be useful when trying to control it from another program
-        pass
+    def get_direction(self, frl) :
+        # frl: front, right, left
+        # 0 : front
+        # 1 : right
+        # 2 : left
+
+        if frl == 0 :
+            pass
+        elif frl == 1 :
+            if self.direction == self.Directions.UP :
+                self.direction = self.Directions.RIGHT
+            elif self.direction == self.Directions.DOWN :
+                self.direction = self.Directions.LEFT
+            elif self.direction == self.Directions.RIGHT :
+                self.direction = self.Directions.DOWN
+            else :
+            # self.direction = self.Directions.LEFT :
+                self.direction = self.Directions.UP
+
+        elif frl == 2 :
+            if self.direction == self.Directions.UP :
+                self.direction = self.Directions.LEFT
+            elif self.direction == self.Directions.DOWN :
+                self.direction = self.Directions.RIGHT
+            elif self.direction == self.Directions.RIGHT :
+                self.direction = self.Directions.UP
+            else :
+                self.direction = self.Directions.DOWN
+
+                """
+        if event.key == pygame.K_UP and self.direction != self.Directions.DOWN :
+            self.direction = self.Directions.UP
+        if event.key == pygame.K_DOWN and self.direction != self.Directions.UP:
+            self.direction = self.Directions.DOWN
+        if event.key == pygame.K_LEFT and self.direction != self.Directions.RIGHT:
+            self.direction = self.Directions.LEFT
+        if event.key == pygame.K_RIGHT and self.direction != self.Directions.LEFT:
+            self.direction = self.Directions.RIGHT
+                """
 
     def move_snake(self) :
         if (self.snake_pos[0] >= 0 and self.snake_pos[1] >= 0 and self.snake_pos[0] <= self.swidth - self.snake_size and self.snake_pos[1] <= self.sheight - self.snake_size and self.snake_pos not in self.snake_body[1:]) :
-            self.snake_pos[0] += self.snake_speed*self.direction[0]
-            self.snake_pos[1] += self.snake_speed*self.direction[1]
+            self.snake_pos[0] += self.snake_speed*self.direction.value[0]
+            self.snake_pos[1] += self.snake_speed*self.direction.value[1]
         else :
             print("Game Over! Restarting the game...")
-            self.reset_game()
+            self.__init__(speed=15, screen_width = 1000, screen_height = 600)
 
     def check_food_eaten(self) :
         if self.snake_pos == self.food_pos :
@@ -94,24 +121,20 @@ class Snake :
             self.snake_body.pop()
 
     def game_loop(self) :
+        print(self.direction)
         self.screen.fill(BLACK)
 
         for event in pygame.event.get() :
             if event.type == pygame.QUIT :
                 pygame.quit()
                 quit()
-            
+
+            """
             if event.type == pygame.KEYDOWN :
-                if event.key == pygame.K_UP and self.direction != [0,1] :
-                    self.direction = [0,-1]
-                if event.key == pygame.K_DOWN and self.direction != [0,-1]:
-                    self.direction = [0,1]
-                if event.key == pygame.K_LEFT and self.direction != [1,0]:
-                    self.direction = [-1,0]
-                if event.key == pygame.K_RIGHT and self.direction != [-1,0]:
-                    self.direction = [1,0]
-        
-        self.get_direction()
+                self.direction = self.get_direction()
+            """
+                    
+#         self.get_direction()
         self.move_snake()
         self.check_food_eaten()
         self.update_snake_body()
@@ -119,8 +142,3 @@ class Snake :
 
         pygame.display.update()
         self.clock.tick(self.game_speed)
-
-
-player = Snake()
-while True:
-    player.game_loop()
